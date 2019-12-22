6fd3f8c22b04d4a3ddbe0e57409466542504faa8
Michael <michaelbao13@gmail>
2019-12-22 13:47:22 -0800

    fin

---
+++ app/config/config.php
@@ -0,0 +1,14 @@
+<?php
+  // DB Params
+  define("DB_HOST", "_YOURHOST_");
+  define("DB_USER", "_YOURUSER_");
+  define("DB_PASS", "_YOURPassWord_");
+  define("DB_NAME", "_YOURDB_");
+
+  // App Root
+  define('APPROOT', dirname(dirname(__FILE__)));
+  // URL Root
+  define('URLROOT', 'http://localhost/showerthoughts');
+  // Site Name
+  define('SITENAME', 'Showerthoughts');
+  
---
+++ app/views/pages/about.php
@@ -0,0 +1,5 @@
+<?php require APPROOT . '/views/inc/header.php'; ?>
+  <h1>About</h1>
+  <p>This is a social network to post your Shower thoughts!</p>
+  <p>App Version: <?php echo $data['version']; ?></p>
+<?php require APPROOT . '/views/inc/footer.php'; ?>
